59795aadeb8acb18622c8607c9959fccdfeb6020
Rakesh Kamitkar <rakeshkamitkar@example.com>
2025-10-23 20:13:05 +0530

    Fix absolute imports in __init__.py for Streamlit deployment

--- source/__init__.py
+++ source/__init__.py
@@ -1,10 +1,9 @@
-
 """
 This package contains core modules for the Ethical AI Bias Detection project.
 """
 
-from .preprocess import preprocess
-from .fairness_check import (
+from source.preprocess import preprocess
+from source.fairness_check import (
     demographic_parity_difference,
     equality_of_opportunity_difference
 )
