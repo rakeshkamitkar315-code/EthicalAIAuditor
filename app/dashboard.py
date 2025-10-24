237e5f1db4c03c2d869307ed7853f11bc42e554d
Rakesh Kamitkar <rakeshkamitkar@example.com>
2025-10-24 11:22:11 +0530

    message

--- app/dashboard.py
+++ app/dashboard.py
@@ -6,7 +6,6 @@ current_dir = os.path.dirname(os.path.abspath(__file__))
 project_root = os.path.abspath(os.path.join(current_dir, '..'))
 sys.path.insert(0, project_root)
 
-
 import streamlit as st
 import joblib
 import pandas as pd
